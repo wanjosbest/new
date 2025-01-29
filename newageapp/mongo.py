236bf358f39980a8314be389443d4ad391f67dd5
wanjosbest <best9ja1@gmail.com>
2025-01-29 13:09:31 -0800

    fffdffd

--- newageapp/mongo.py
+++ newageapp/mongo.py
@@ -10,9 +10,9 @@ encoded_username = quote_plus(username)
 encoded_password = quote_plus(password)
 """
 # Replace with your MongoDB connection string
-CONNECTION_STRING = "mongodb+srv://wanjos:0903620Wanjos{encoded_password}@newagedatabase.40ybp.mongodb.net/?retryWrites=true&w=majority&appName=newagedatabase"
+CONNECTION_STRING = "mongodb+srv://wanjos:0903620Wanjos@newagedatabase.40ybp.mongodb.net/?retryWrites=true&w=majority&appName=newagedatabase"
 # Connect to the MongoDB cluster
-client = MongoClient(CONNECTION_STRING, tls = True, tlsAllowInvalidCertificates = True)
+client = MongoClient(CONNECTION_STRING,ssl=False)
 
 # Access the specific database and collection
 db = client['newageadatabase']  # Database name
